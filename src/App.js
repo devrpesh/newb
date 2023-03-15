971950d52de4ddf4ac7a004650c7c9fe167fa959
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2023-03-15 21:31:59 +0600

    NEWB

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import disclaimer from './resources/disclaimer.mp3';
+import profile from './resources/profile.jpg';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 import './App.css';
@@ -9,7 +10,7 @@ class App extends Component {
     return (
       <div id="main" >
         <div id="container-1">
-          <img id="profile" className='img-fluid' alt="Profile" src="https://scontent.fdac20-1.fna.fbcdn.net/v/t39.30808-1/294531711_593834018777264_4483344312901387270_n.jpg?stp=dst-jpg_s320x320&amp;_nc_cat=103&amp;ccb=1-7&amp;_nc_sid=c6021c&amp;_nc_ohc=7M2ivkNL450AX_XRpxg&amp;_nc_ht=scontent.fdac20-1.fna&amp;oh=00_AfDd3cFAGbppriwpMDBxCj1AW0AhMUJPMSJGUa32nXT6jg&amp;oe=6411A225"></img>
+          <img id="profile" className='img-fluid' alt="Profile" src={profile}></img>
           <h1>NEWB</h1>
           <p>Gaming Content Creator, Gaali Artist</p>
           <button onClick={this.playDisclaimer} className='btn btn-danger' style={{ marginBottom: "1rem" }}>Disclaimer</button>
